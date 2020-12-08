f8e691fd1a8410d476c88ea408036ecfe5abcab4
eonsubae <eonsubae@naver.com>
2020-12-08 15:27:31 +0900

    Storyboards and Errors

--- My First App/ViewController.swift
+++ My First App/ViewController.swift
@@ -1,9 +1,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var centerLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .green
+        centerLabel.text = "I like cheese!"
+        centerLabel.textColor = .white
     }
 
 
