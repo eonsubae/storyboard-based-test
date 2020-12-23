e4e88492c61f23e3f99929dcfd1fe1a69a519e08
eonsubae <eonsubae@naver.com>
2020-12-23 14:45:03 +0900

    문자열의 다양한 기능

--- yagom-swift.playground/Contents.swift
+++ yagom-swift.playground/Contents.swift
@@ -1,33 +1,48 @@
 import UIKit
 
-// Charactert는 문자 한 개를 저장하는 데이터 타입이다
-let alphabetA: Character = "A"
-print(alphabetA) // A
+// 문자열 타입의 다양한 기능
+let hello: String = "Hello"
+let esbae: String = "esbae"
+var greeting: String = hello + " " + esbae + "!"
+print(greeting) // Hello esbae!
 
-let commandCharacter: Character = "❤️"
-print(commandCharacter) // ❤️ 스위프트는 유니코드9를 사용하므로 영어는 물론 유니코드에서 지원하는 모든 언어 및 이모티콘을 사용할 수 있다
+greeting = hello
+greeting += " "
+greeting += esbae
+greeting += "!"
+print(greeting) // Hello esbae!
 
-// String은 나열된 문자인 문자열을 저장하는 데이터 타입이다 캐릭터와 마찬가지로 유니코드9를 사용한다
-let name: String = "esbae"
+// 연산자를 통한 문자열 비교
+var isSameString: Bool = false
 
-// 이니셜라이저를 사용해 빈문자열을 생성
-// var 키워드를 사용해 변수를 생성했으므로 문자열의 수정이 가능하다
-var introduce: String = String()
+isSameString = hello == "Hello"
+print(isSameString) // true
 
-// append메서드를 사용하여 문자열을 후방으로 이어붙일 수 있다
-introduce.append("제 이름은")
+isSameString = hello == "hello"
+print(isSameString) // false
 
-// + 연산자를 통해서도 문자열을 이어 붙일 수 있다
-introduce = introduce + " " + name + "입니다."
+// 메서드를 통한 접두어, 접미어 확인
+var hasPrefix: Bool = false
+hasPrefix = hello.hasPrefix("He") // He라는 접두어를 가지고 있는지를 확인해서 불리언 타입을 할당
+print(hasPrefix) // true
 
-print(introduce) // 제 이름은 esbae입니다.
+var hasSuffix: Bool = false
+hasSuffix = hello.hasSuffix("llo") // llo라는 접미어를 가지고 있는지를 확인해서 불리언 타입을 할당
+print(hasSuffix) // true
 
-// 문자열의 길이를 세기
-print("name의 글자 수 : \(name.count)") // 5
+// 메서드를 통한 대소문자 변환
+var convertedString: String = ""
+convertedString = hello.uppercased()
+print(convertedString) // HELLO
 
-// 빈 문자열인지 확인
-print("introduce가 비어있습니까?: \(introduce.isEmpty)") // false
+convertedString = hello.lowercased()
+print(convertedString) // hello
 
-// 유니코드의 스칼라값을 사용하면 값에 해당하는 표현이 출력된다
-let unicodeScalarValue: String = "\u{2665}"
-print(unicodeScalarValue) // ♥
+// 코드상에서 여러 줄의 문자열을 쓰기
+greeting = """
+안녕하세요 저는 배언수입니다.
+스위프트 잘하고 싶어요!
+잘 부탁합니다!
+"""
+
+print(greeting)
