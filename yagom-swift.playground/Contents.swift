9af8652022c876f69a3083c73891f35673692ada
eonsubae <eonsubae@naver.com>
2020-12-23 14:10:49 +0900

    불리언

--- yagom-swift.playground/Contents.swift
+++ yagom-swift.playground/Contents.swift
@@ -1,8 +1,5 @@
 import UIKit
 
-var integer: Int = -100 // Int에는 음수 할당이 가능
-let unsignedInt: UInt = 50 // UInt에는 양수만 할당 가능
-let largeInteger: Int64 = Int64.max // 64비트의 정수
-let smallUnsignedInteger: UInt8 = UInt8.max // 8비트의 양수인 정수만 할당 가능
-//integer = unsignedInt // UInt를 Int에 할당할 수 없으므로 에러가 발생
-integer = Int(unsignedInt) // 새로운 Int값을 생성해 할당하는 것은 가능
+var boolean: Bool = true
+boolean.toggle() // toggle을 호출한 변수의 true와 false를 반전시킴
+print(boolean) // false
