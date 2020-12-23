b904f7d349ae4b7662877c2691cfa1575ce9c3d1
eonsubae <eonsubae@naver.com>
2020-12-23 14:03:28 +0900

    상수

--- yagom-swift.playground/Contents.swift
+++ yagom-swift.playground/Contents.swift
@@ -1,10 +1,11 @@
 import UIKit
 
-var name: String = "esbae" // 타입 지정
+let name: String = "esbae" // let으로 상수를 선언
 var age: Int = 30 // 타입 지정
 var job = "iOS Programmer" // 타입 추론 String
-var height = 181.5 // 타입 추론 Double
+let height = 181.5 // let으로 상수를 선언
 age = 31 // var는 변수이므로 값을 변경할 수 있다
 job = "Writer"
+// name = "sjfld" let은 상수이므로 변경하려하면 에러가 발생
 
 print("저의 이름은 \(name)이고, 나이는 \(age)세이며, 직업은 \(job)입니다. 비밀이지만 키는 \(height)센티미터입니다.")
