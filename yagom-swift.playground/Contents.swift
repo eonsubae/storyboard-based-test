f80a227317eaef140d0fa9aa427c942cd2b60b51
eonsubae <eonsubae@naver.com>
2020-12-25 09:37:30 +0900

    컬렉션에서 임의의 요소 추출, 뒤섞기

--- yagom-swift.playground/Contents.swift
+++ yagom-swift.playground/Contents.swift
@@ -1,53 +1,20 @@
 import UIKit
 
 // 컬렉션(Array, Dictionary, Set)
-// 튜플 이외에도 데이터를 묶어서 저장하고 관리할 수 있는 타입인 컬렉션 타입이 있다
+// 컬렉션에서 임의의 요소 추출, 뒤섞기
 
-// Set
-// 같은 타입의 데이터를 순서 없이 하나의 묶음으로 저장하는 컬렉션
-// 순서가 중요하지 않거나 각 요소가 유일한 값이어야 하는 경우에 사용
+var array: [Int] = [0, 1, 2, 3, 4]
+var set: Set<Int> = [0, 1, 2, 3, 4]
+var dictionary: [String: Int] = ["a": 1, "b": 2, "c": 3]
+var string: String = "string"
 
-// 세트의 선언과 생성
-// 세트는 배열과 달리 축약형이 없다
-var names: Set<String> = Set<String>() // 빈 세트 생성
-var names2: Set<String> = [] // 빈 세트 생성
+print(array.randomElement()) // 임의의 요소가 추출됨
+print(array.shuffled()) // 배열 안의 요소가 뒤섞여서 출력된다. 이 때 array 내부의 요소는 그대로다
+print(array) // [0, 1, 2, 3, 4]
+array.shuffle() // 배열 자체를 뒤섞기
+print(array) // 뒤섞인 배열이 출력된다
 
-// Array와 마찬가지로 대괄호를 사용
-// 타입 추론시에는 컴파일러가 Set이 아닌 Array로 지정하므로 주의하자
-var names3: Set<String> = ["esbae", "cs", "yh", "esbae"]
-
-print(names3.isEmpty) // false
-print(names3.count) // 3
-
-names3.insert("jenny")
-print(names3.count) // 4
-print(names3.remove("cs")) // cs
-print(names3.remove("john")) // nil
-
-/* 집합연산 */
-let englishClassStudents: Set<String> = ["john", "cs", "esbae"]
-let koreanClassStudents: Set<String> = ["jenny", "esbae", "cs", "hana", "ms"]
-
-// 교집합
-let intersectSet: Set<String> = englishClassStudents.intersection(koreanClassStudents) // {"esbae", "cs"}
-
-// 여집합
-let symmetricDiffSet: Set<String> = englishClassStudents.symmetricDifference(koreanClassStudents) // {"john", "jenny", "hana", "ms"}
-
-// 합집합
-let unionSet: Set<String> = englishClassStudents.union(koreanClassStudents) // {"john", "cs", "esbae", "jenny", "hana", "ms"}
-
-// 차집합
-let subtractSet: Set<String> = englishClassStudents.subtracting(koreanClassStudents) // {"john"}
-
-print(unionSet.sorted()) // ["cs", "esbae", "hana", "jenny", "john", "ms"]
-
-/* 포함관계 연산 */
-let bird: Set<String> = ["비둘기", "닭", "기러기"]
-let mammal: Set<String> = ["사자", "호랑이", "곰"]
-let animal: Set<String> = bird.union(mammal) // bird와 mammal의 합집합
-
-print(bird.isDisjoint(with: mammal)) // 서로 배타적인지 true
-print(bird.isSubset(of: animal)) // 새가 동물의 부분집합인가? true
-print(animal.isSuperset(of: mammal)) // 동물은 포유류의 전체집합인가? true
-print(animal.isSuperset(of: bird)) // 동물은 새의 전체집합인가? true
+print(set.shuffled()) // 세트를 뒤섞으면 배열로 반환된다
+// set.shuffle() 오류가 발생. 세트는 순서가 없으므로 스스로 뒤섞기가 불가능
+print(dictionary.shuffled()) // 딕셔너리를 뒤섞으면 (키, 값)이 쌍을 이룬 튜플의 배열이 반환된다
+print(string.shuffled()) // String도 컬렉션이다. 순서가 뒤섞인 문자열의 배열이 출력된다
