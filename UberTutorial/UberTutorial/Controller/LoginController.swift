f8cead3d67cc3a9ffc92994bafa6bb9c4e523460
eonsubae <eonsubae@naver.com>
2020-12-31 12:30:09 +0900

    Project Setup

---
+++ UberTutorial/UberTutorial/Controller/LoginController.swift
@@ -0,0 +1,19 @@
+//
+//  LoginController.swift
+//  UberTutorial
+//
+//  Created by ma-esb on 2020/12/31.
+//
+
+import UIKit
+
+class LoginController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .red
+    }
+
+
+}
