699be4889c423f36365441b1c70ff3d26cd3cfc1
eonsubae <eonsubae@naver.com>
2021-01-07 13:47:47 +0900

    얼럿

---
+++ Web/Web/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Web
+//
+//  Created by KRITSSEAN on 2021/01/07.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var txtUrl: UITextField!
+    @IBOutlet var myWebView: WKWebView!
+    @IBOutlet var myActivityIndicator: UIActivityIndicatorView!
+    
+    func loadWebPage(_ url: String) {
+        let myUrl = URL(string: url)
+        let myRequest = URLRequest(url: myUrl!)
+        myWebView.load(myRequest)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        loadWebPage("http://google.com")
+    }
+
+
+    @IBAction func btnGotoUrl(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnGoSite1(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnGoSite2(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnLoadHtmlString(_ sender: UIButton) {
+    }
+    
+    @IBAction func btnLoadHtmlFile(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func btnStop(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func btnReload(_ sender: UIBarButtonItem) {
+    }
+    
+    
+    @IBAction func btnGoBack(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func btnGoForward(_ sender: UIBarButtonItem) {
+    }
+    
+}
+
