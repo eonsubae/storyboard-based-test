5dbb15929a18f203e11eee04512a7d4a3c39b0cd
eonsubae <eonsubae@naver.com>
2021-01-23 23:36:30 +0900

    핀치 - 레이블

---
+++ PinchGesture/PinchGesture/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  PinchGesture
+//
+//  Created by KRITSSEAN on 2021/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var txtPinch: UILabel!
+    
+    var initialFontSize: CGFloat!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        let pinch = UIPinchGestureRecognizer(target: self, action: #selector(self.doPinch(_:)))
+        self.view.addGestureRecognizer(pinch)
+    }
+
+    @objc func doPinch(_ pinch: UIPinchGestureRecognizer) {
+        if (pinch.state == UIGestureRecognizer.State.began) {
+            initialFontSize = txtPinch.font.pointSize
+        } else {
+            txtPinch.font = txtPinch.font.withSize(initialFontSize * pinch.scale)
+        }
+    }
+
+}
+
