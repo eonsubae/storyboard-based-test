854a265f33f6389d36f63fc9020d78a0849ce9af
eonsubae <eonsubae@naver.com>
2021-02-01 23:55:05 +0900

    미소유참조와 클로저의 강한참조 순환

--- yagom-swift.playground/Contents.swift
+++ yagom-swift.playground/Contents.swift
@@ -140,6 +140,152 @@ var roomWeak: Room? = Room(number: "505") // Room 인스턴스의 참조 횟수 
 room?.host = es // PersonB 인스턴스의 참조 횟수 : 1 (weak이므로 증가x)
 es?.room = room // Room 인스턴스의 참조 횟수 : 2
 
-es = nil // Person 인스턴스의 참조 횟수 : 1
-room = nil  // Room 인스턴스의 참조 횟수 : 0
+es = nil // PersonB 인스턴스의 참조 횟수 : 0
+room = nil  // Room 인스턴스의 참조 횟수 : 1
 // Room 505 is being deinitialized
+
+/* 미소유참조Unknown Reference */
+// 자신이 참조하는 인스턴스가 항상 메모리에 있을 것이라고 전제하고 동작한다
+// 만약 참조하는 인스턴스가 메모리에서 해제도더라도 nil을 자동으로 할당해주지 않는다
+// 따라서 옵셔널일 필요가 없다. 그렇지만 메모리에서 해제된 인스턴스에 접근하려하면 런타임 오류로 프로세스가 강제종료된다
+// 따라서 참조하는 동안 메모리에서 해제되지 않는다는 확신이 있을 때만 사용한다
+// unowned 키워드를 쓰면 변수(상수)나 프로퍼티는 자신이 참조하는 인스턴스를 미소유참조한다
+
+/* 미소유 참조의 사용법 */
+// 사람과 신용카드의 관계를 생각하면 쉽다
+// 사람이 신용카드를 소지하지 않을 수는 있지만, 신용카드는 명의자가 반드시 있어야만 한다
+class PersonC {
+    let name: String
+    
+    var card: CreditCard? // 사람은 신용카드가 없어도 된다
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+class CreditCard {
+    let number: UInt
+    unowned let owner: PersonC // 카드는 소유자가 반드시 있어야 한다
+    
+    init(number: UInt, owner: PersonC) {
+        self.number = number
+        self.owner = owner
+    }
+    
+    deinit {
+        print("Card #\(number) is being deinitialized")
+    }
+}
+
+var js: PersonC? = PersonC(name: "JS") // PersonC 인스턴스의 참조 횟수 : 1
+
+if let person: PersonC = js {
+    person.card = CreditCard(number: 1004, owner: person) // CreditCard 인스턴스의 참조 횟수 : 1
+    // PersonC 인스턴스의 참조 횟수 : 1, 미소유참조된 인스턴스는 참조 횟수가 증가하지 않는다
+}
+
+js = nil // PersonC 인스턴스의 참조 횟수 : 0
+// CreditCard 인스턴스의 참조 횟수 : 0
+// JS is being deinitialized
+// Card #1004 is being deinitialized
+
+/* 미소유참조와 암시적 추출 옵셔널 프로퍼티 */
+// 서로 참조해야 하는 프로퍼티에 값이 꼭 있어야 하면서도
+// 한번 초기화하면 그 이후에는 nil을 할당할 수 없는 조건을 갖추어야 하는 경우
+class Company {
+    let name: String
+    
+    // 암시적 추출 옵셔널 프로퍼티(강한참조)
+    var ceo: CEO!
+    
+    init(name: String, ceoName: String) {
+        self.name = name
+        self.ceo = CEO(name: ceoName, company: self)
+    }
+    
+    func introduce() {
+        print("\(name)의 CEO는 \(ceo.name)입니다.")
+    }
+}
+
+class CEO {
+    let name: String
+    
+    // 미소유참조 상수 프로퍼티(미소유참조)
+    unowned let company: Company
+    
+    init(name: String, company: Company) {
+        self.name = name
+        self.company = company
+    }
+    
+    func introduce() {
+        print("\(name)는 \(company.name)의 CEO입니다.")
+    }
+}
+
+let company: Company = Company(name: "테슬라", ceoName: "머스크")
+company.introduce() // 테슬라의 CEO는 머스크입니다.
+company.ceo.introduce() // 머스크는 테슬라의 CEO입니다.
+
+/* 클로저의 강한참조 순환 */
+// 강한참조 순환 문제는 클로저가 인스턴스의 프로퍼티일 때, 클로저의 값 획득 특성 때문에도 발생한다
+// 예를 들어, self.someProperty처럼 인스턴스의 프로퍼티에 접근할 때, 클로저가 self를 획득하므로 강한참조 순환이 발생한다
+// 이는 클로저의 획득 목록을 통해 해결할 수 있다
+class PersonD {
+    let name: String
+    let hobby: String?
+    
+    lazy var introduce: () -> String = {
+        var introduction: String = "My name is \(self.name)"
+        
+        guard let hobby = self.hobby else {
+            return introduction
+        }
+        
+        introduction += " "
+        introduction += "My hobby is \(hobby)"
+        
+        return introduction
+    }
+    
+    init(name: String, hobby: String? = nil) {
+        self.name = name
+        self.hobby = hobby
+    }
+    
+    deinit {
+        print("\(name) is being deinitialized")
+    }
+}
+
+var bs: PersonD? = PersonD(name: "bs", hobby: "eating")
+print(bs?.introduce()) // My name is bs. My hobby is eating.
+// 자기소개를 하기위해 introduce를 호출하면 자신의 내부에 있는 참조 타입 변수를 획득
+bs = nil // 디이니셜라이저 호출이 안됨. 메모리 누수중
+
+/* 획득목록Capture list*/
+// 클로저 내부에서 참조 타입을 획득하는 규칙을 제시하는 기능
+// 위의 문제는 클로저 내부의 self를 약한참조로 지정해서 문제를 해결가능
+// 매개변수 목록 이전 위치에 작성하며, 참조 방식과 참조 대상을 대괄호로 둘러싼 목록 형식으로 작성
+// 획득목록 뒤에는 in 키워드를 써준다
+class SimpleClass {
+    var value: Int = 0
+}
+
+var x: SimpleClass? = SimpleClass()
+var y = SimpleClass()
+
+let closure = { [weak x, unowned y] in
+    print(x?.value, y.value)
+}
+
+x = nil
+y.value = 10
+
+closure() // nil 10
